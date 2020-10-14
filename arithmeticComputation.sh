a3f1b4aaa2719a04796a966888410e2b30fc5384
kamchamswarna <kamchamswarna@gmail.com>
2020-10-14 13:29:32 +0530

    store the result in dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -6,9 +6,31 @@ read -p "enter value of a:" a
 read -p "enter value of b" b
 read -p "enter value of c:" c
 
-num=$(( $a % $b + $c ));
+first=$(( $a + $b * $c ));
 
-echo "compute (a%b+c) : $num"
+echo "compute (a+b*c) : $first"
+
+second=$(( $a * $b + $c ))
+
+echo "compute (a*b+c):$second"
+
+third=$(( $c + $a / $b ))
+
+echo "compute (c+a/b) : $third "
+
+four=$(( $a % $b + $c ))
+
+echo "compute (a+b+c) : $four "
+
+counter=0
+
+result[$((counter++))]=$first
+result[$((counter++))]=$second
+result[$((counter++))]=$third
+result[$((counter++))]=$four
+echo ${result[@]}
+echo ${!result[@]}
+
 
 
 
